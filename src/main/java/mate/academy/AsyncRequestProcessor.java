9fe18cff47c5395c84be50ce87909e0867ed9ad2
smagles <lesialist@icloud.com>
2025-08-08 12:38:46 +0200

    Asynchronous user request processing and caching

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,16 +1,32 @@
 package mate.academy;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
 
 public class AsyncRequestProcessor {
     private final Executor executor;
+    private Map<String, UserData> cache = new ConcurrentHashMap<>();
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        if (cache.containsKey(userId)) {
+            return CompletableFuture.supplyAsync(() -> (cache.get(userId)), executor);
+        }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        return CompletableFuture.supplyAsync(() -> cache.computeIfAbsent(userId,
+                id -> new UserData(userId,
+                        "details=Details for " + userId)), executor);
     }
 }
+
+
